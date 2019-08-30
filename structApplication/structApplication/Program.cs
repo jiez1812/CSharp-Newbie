25d864dc9bbfbec3652fad4ba0c4c2f79643d0fe
Jiez Tee <jieztee@gmail.com>
2019-08-30 09:27:42 +0800

    struct application

---
+++ structApplication/structApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace structApplication
+{
+    struct Person
+    {
+        public string job;
+        public string name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person b;
+            b.name = "Bob";
+            b.job = "Chemist";
+
+            Console.WriteLine("b.name = {0}", b.name);
+            Console.WriteLine("b.job = {0}", b.job);
+        }
+    }
+}
