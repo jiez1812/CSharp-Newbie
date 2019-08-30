9baadac11acffd7742aed7a6ca1c8b7f045b8c6d
Jiez Tee <jieztee@gmail.com>
2019-08-30 18:05:07 +0800

    Counter App

---
+++ Counter App/Counter App/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Counter_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.count_label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // count_label
+            // 
+            this.count_label.AutoSize = true;
+            this.count_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.count_label.Location = new System.Drawing.Point(115, 43);
+            this.count_label.Name = "count_label";
+            this.count_label.Size = new System.Drawing.Size(0, 36);
+            this.count_label.TabIndex = 0;
+            this.count_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button1.Location = new System.Drawing.Point(66, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button2.Location = new System.Drawing.Point(66, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Minus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 237);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.count_label);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Counter App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label count_label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Counter App/Counter App/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Counter_App
+{
+    public partial class Form1 : Form
+    {
+        int count_num = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            count_num += 1;
+            count_label.Text = Convert.ToString(count_num);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            count_label.Text = Convert.ToString(count_num);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            count_num -= 1;
+            count_label.Text = Convert.ToString(count_num);
+        }
+    }
+}
